176ab7d2371f38ea18f3743a3038989994374c3b
Casper Sjöström <ca8416sj-s@student.lu.se>
2026-02-16 17:07:05 +0000

    Comment

--- db_models/menu_model.py
+++ db_models/menu_model.py
@@ -2,8 +2,9 @@ from typing import Optional
 from db_models.base_model import BaseModel_DB
 from sqlalchemy.orm import mapped_column, Mapped
 
+
 class menu_item(BaseModel_DB):
-    __tablename__="Menu"
+    __tablename__ = "Menu"
 
     id: Mapped[str] = mapped_column(primary_key=True, init=False)
     name: Mapped[str] = mapped_column()
@@ -11,3 +12,6 @@ class menu_item(BaseModel_DB):
     description: Mapped[str] = mapped_column()
     hasRunOut: Mapped[bool] = mapped_column(init=False, default=False)
     ingredients: Mapped[list[str]] = mapped_column()
+
+
+# gjreogjreoigjoi
