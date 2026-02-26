b8e556cb565572da04b25ba689f68bfe37aefd1c
Casper Sjöström <ca8416sj-s@student.lu.se>
2026-02-26 17:52:46 +0000

    Order2

--- routes/order_router.py
+++ routes/order_router.py
@@ -2,17 +2,21 @@ from fastapi import APIRouter, HTTPException, status
 from api_schemas.order_schema import OrderCreate, OrderRead, OrderUpdate
 from database import DB_dependency
 from db_models.order_model import Order_DB
-
+from datetime import datetime
 
 order_router = APIRouter()
 
 
 @order_router.post("/", response_model=OrderRead)
 def create_fruit(order_data: OrderCreate, db: DB_dependency):
+    time = datetime.now()
     order = Order_DB(
-        items=order_data.items,
-        adress=order_data.address,
-        note=order_data.note,
+        items = order_data.items,
+        placedat = time
+        status = 'Pending'
+        total = 0
+        adress = order_data.address,
+        note = order_data.note,
     )
     db.add(order)
     db.commit()
