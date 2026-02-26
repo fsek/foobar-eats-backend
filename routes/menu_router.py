39461c2fae9ef21021e45f229be064de75cc7545
Casper Sjöström <ca8416sj-s@student.lu.se>
2026-02-26 16:52:33 +0000

    Namn menu_router

--- routes/menu_router.py
+++ routes/menu_router.py
@@ -1,21 +1,25 @@
 from fastapi import APIRouter, HTTPException, status
 from api_schemas.menu_schema import MenuRead
 from database import DB_dependency
-from db_models.menu_model import menu_item
+from db_models.menu_model import menu_DB
 
-menu_router = APIRouter() 
+menu_router = APIRouter()
+
 
 @menu_router.get("/", response_model=list[MenuRead])
 def get_all_items(db: DB_dependency):
-    fruit = db.query(menu_item).all()
-    return fruit
+    menu_items = db.query(menu_DB).all()
+    return menu_items
+
 
 @menu_router.get("/{fruit_id}", response_model=MenuRead)
 def get_item(fruit_id: int, db: DB_dependency):
-    fruit = db.query(menu_item).filter_by(id=fruit_id).one_or_none()
-    if fruit is None:
+    menu_item = db.query(menu_DB).filter_by(id=fruit_id).one_or_none()
+    if menu_item is None:
         raise HTTPException(status.HTTP_404_NOT_FOUND)
-    return fruit
+    return menu_item
+
+
 """
 @menu_router.post("/", response_model=MenuRead)
 def create_fruit(fruit_data: FruitCreate, db: DB_dependency):
