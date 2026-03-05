d61fb98cd91a11663a93a880fec9ee7a07bc7906
Casper Sjöström <ca8416sj-s@student.lu.se>
2026-03-05 18:28:52 +0000

    la till router

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .example_router import fruit_router
 from .menu_router import menu_router
+from .order_router import order_router
 
 # Add new routers here
 main_router = APIRouter()
 
 main_router.include_router(fruit_router, prefix="/fruit", tags=["fruit"])
 main_router.include_router(menu_router, prefix="/menu", tags=["menu"])
+main_router.include_router(order_router, prefix="/order", tags=["order"])
